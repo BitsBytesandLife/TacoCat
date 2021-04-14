e323a0f4f2bf2495222389db9a873879b4d438dc
BK <brian_j_king@me.com>
2021-04-14 06:17:28 -0500

    add sweetAlert2

--- js/site.js
+++ js/site.js
@@ -34,13 +34,17 @@ function reverseString() {
 
 function palindromeCheck(origrnalString, reverseString) {
     let palindromeCheckResult = "";
+    let palindromeBool = false;
     //if the words are equal the its palindrome
     //it s not a palindromeCheckResult 
     if (reverseString == origrnalString) {
-        palindromeCheckResult = "This is vaild palindrome."
+        palindromeCheckResult = "This is vaild palindrome. TacoCat owes you 2 tacos"
+        //palindromeBool = true;
     } else {
-        palindromeCheckResult = "This is Not vaild palindrome."
+        palindromeCheckResult = "This is Not vaild palindrome. Sorry no tacos for you"
     }
+
+    //palindromeSuccess(palindromeBool, userWord);
     // return the result
     return palindromeCheckResult;
 }
@@ -48,6 +52,39 @@ function palindromeCheck(origrnalString, reverseString) {
 function stringLengthCheck(orginalStringLen) {
     if (orginalStringLen < 2) {
         //TODO add SweetAlert2
+        // alert("Your String Needs to have at least 2 leters or numbners")
+        Swal.fire(
+            'Error!',
+            'Your String Needs to have at least 2 leters or numbers!',
+            'error'
+        );
+
+    }
+}
+
+function palindromeSuccess(validatePalindrome, validateString) {
+
+    if (validateString < 2) {
+        //TODO add SweetAlert2
         alert("Your String Needs to have at least 2 leters or numbners")
+        Swal.fire(
+            'Error!',
+            'Your String Needs to have at least 2 leters or numbers!',
+            'error'
+        );
+    }
+
+    if (validatePalindrome === true) {
+        Swal.fire(
+            'This is Palindrome!',
+            'TACOCAT owes you two tacos!',
+            'success'
+        );
+    } else {
+
+        Swal.fire(
+            'Not a Palindrome!',
+            'No Tacos for you!',
+            'error');
     }
 }
